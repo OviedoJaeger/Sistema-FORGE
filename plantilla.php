5a496de3ecca0c40bc0aa7a0183126c1b2d4470f
OviedoJaeger <106565437+OviedoJaeger@users.noreply.github.com>
2022-06-07 17:12:04 -0500

    Actualización de plantilla.php
    Ajustamos la plantilla para que el sistema comience a trabajar con el registro de usuarios

--- plantilla.php
+++ plantilla.php
@@ -105,12 +105,6 @@ CUERPO DOCUMENTO
 
       if($_GET["ruta"] == "inicio" ||
          $_GET["ruta"] == "usuarios" ||
-         $_GET["ruta"] == "categorias" ||
-         $_GET["ruta"] == "productos" ||
-         $_GET["ruta"] == "clientes" ||
-         $_GET["ruta"] == "ventas" ||
-         $_GET["ruta"] == "crear-venta" ||
-         $_GET["ruta"] == "reportes" ||
          $_GET["ruta"] == "salir"){
 
         include "modulos/".$_GET["ruta"].".php";
